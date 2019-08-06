28e769e2a98cdb85c72aabf0003d41928f51c380
JustinRobertKing <kingj4@spu.edu>
2019-08-06 14:47:23 -0700

    all works

--- build/bucketSort.js
+++ build/bucketSort.js
@@ -1,11 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function bucketSort(arr, k = 10) {
+    let result = [];
     const buckets = [];
     for (let i = 0; i < k; i++) {
         buckets.push(new Array());
     }
     let max = Math.max(...arr);
     for (let unsortedNumber of arr) {
-        let index = Math.floor(unsortedNumber / (max + 1));
+        let index = Math.floor((unsortedNumber / (max + 1)) * k);
+        buckets[index].push(unsortedNumber);
+    }
+    for (let bucket of buckets) {
+        insertionSort(bucket);
+        result = [...result, ...bucket];
+    }
+    return result;
+}
+exports.default = bucketSort;
+function insertionSort(arr) {
+    let i = 1;
+    while (i < arr.length) {
+        let j = i;
+        // console.log(`j and i:`, j, i)
+        while (j > 0 && arr[j] < arr[j - 1]) {
+            let x = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = x;
+            // console.log(`swapping numbers:`, arr[j-1], arr[j]);
+            j -= 1;
+        }
+        i++;
     }
 }
 //# sourceMappingURL=bucketSort.js.map
--- src/bucketSort.ts
+++ src/bucketSort.ts
@@ -1,10 +1,37 @@
-function bucketSort(arr: number[], k: number = 10): number[] {
-	const buckets = []
+export default function bucketSort(arr: number[], k: number = 10): number[] {
+	let result: number[] = [];
+	const buckets = [];
 	for (let i = 0; i < k; i++) {
-		buckets.push(new Array())
+		buckets.push(new Array());
 	}
-	let max = Math.max(...arr)
+	let max = Math.max(...arr);
 	for (let unsortedNumber of arr) {
-		let index = Math.floor((unsortedNumber / (max + 1)) * k)
+		let index = Math.floor((unsortedNumber / (max + 1)) * k);
+		buckets[index].push(unsortedNumber);
 	}
+	for (let bucket of buckets) {
+		insertionSort(bucket);
+		result = [...result, ...bucket]
+	}
+	return result;
+}
+
+
+
+
+
+function insertionSort<T>(arr: Array<T>): void {
+  let i: number = 1
+  while (i < arr.length) {
+    let j: number = i
+    // console.log(`j and i:`, j, i)
+    while (j > 0 && arr[j] < arr[j-1]) {
+      let x: T = arr[j]
+      arr[j] = arr[j-1]
+      arr[j-1] = x
+      // console.log(`swapping numbers:`, arr[j-1], arr[j]);
+      j -= 1;
+    }
+    i++;
+  }
 }
